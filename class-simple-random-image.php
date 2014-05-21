b444ddc20ef71187761582129f4b35e2a21515b3
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-05-21 13:54:51 -0500

    Rename the method to generate a random object from the library and add some methods to handle the image

--- class-simple-random-image.php
+++ class-simple-random-image.php
@@ -1,26 +1,47 @@
 <?php
 class Simple_Random_Image{
 
-    private $imageObject;
+    private $dataObject;
+    private $image;
 
     public function __construct(){
-        $this->imageObject = null;
-        $this->random_image_from_library();
+        $this->initialize();
+        $this->generate();
     }
 
-    private function random_image_from_library(){
+    private function initialize(){
+        $this->dataObject = null;
+        $this->image = array();
+
+        $this->image["url"] = "";
+        $this->image["alt"] = "";
+        $this->image["title"] = "";
+        $this->image["height"] = 0;
+        $this->image["width"] = 0;
+    }
+
+    public function generate(){
         $args = array(
             'numberposts'       => 1,
             'orderby'           => 'rand',
             'post_type'         => 'attachment',
-            'post_mime_type'    =>'image',
+            'post_mime_type'    => 'image',
             'post_status'       => null,
             'post_parent'       => null
         );
 
         $attachments = get_posts( $args );
         if( $attachments ){
-            $this->imageObject = $attachments[0];
+            $this->dataObject = $attachments[0];
+        }
+        echo "<pre>";
+        var_dump($this->dataObject);
+        echo "</pre>";
+    }
+
+    public function get( $size = "medium", $value = "all" ){
+        if( $value === "all" ){
+            return $this->image;
         }
     }
 }
